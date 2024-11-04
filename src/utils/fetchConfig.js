0e8ddde7160580f63d013bc1b878f5587d742295
kateewrightt <wrighk@outlook.com>
2024-11-04 21:56:15 +1100

    Select API URL based on prod/dev environment

--- src/utils/fetchConfig.js
+++ src/utils/fetchConfig.js
@@ -1,5 +1,8 @@
 export async function fetchConfig() {
   const response = await fetch("/config.json");
   const config = await response.json();
-  return config.backendAPI;
+
+  // Choose the backend API based on NODE_ENV
+  const env = process.env.NODE_ENV || "development";
+  return env === "production" ? config.backendAPI.production : config.backendAPI.development;
 }
