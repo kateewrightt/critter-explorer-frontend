bff0ca81a8ca26df15b29a8eb80c036c7aba4f37
kateewrightt <wrighk@outlook.com>
2024-11-19 14:06:49 +1100

    make homepage layout responsive for image positioning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import "./App.css";
-import flick from "./assets/flick.png";
+import flick from "./assets/images/flick.png";
+import cj from "./assets/images/CJ.png";
 import Search from "./components/CitySearch";
-// import PageCount from "./components/PageCount";
 
 function App() {
   const handleOnSearchChange = (searchData) => {
@@ -13,28 +13,33 @@ function App() {
   return (
     <div className="App">
       <Container className="text-center mt-5">
-        <Row>
-          <Col className="image-col">
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <img src={flick} alt="Flick" className="image" />
-              {/* <PageCount /> */}
-            </div>
+
+        {/* Small screens: CJ and Flick side by side above the title */}
+        <div className="d-flex d-md-none justify-content-center image-container">
+          <img src={cj} alt="CJ" className="cj-image" />
+          <img src={flick} alt="Flick" className="flick-image" />
+        </div>
+
+        {/* Title row with separate CJ and Flick images for larger screens */}
+        <Row className="align-items-center header-row">
+          <Col md="2" className="d-none d-md-flex justify-content-start image-col">
+            <img src={cj} alt="CJ" className="cj-image" />
+          </Col>
+
+          <Col xs="12" md="8" className="text-center">
+            <h1 className="header">Critter Explorer</h1>
+            <p className="subheader">
+              Find what ACNH critters are out and about in your city!
+            </p>
+          </Col>
+
+          <Col md="2" className="d-none d-md-flex justify-content-end image-col">
+            <img src={flick} alt="Flick" className="flick-image" />
           </Col>
         </Row>
+
         <Row>
           <Col>
-            <div className="center-content">
-              <h1 className="header">Critter Explorer</h1>
-              <p className="subheader">
-                Find what ACNH critters are out and about in your city!
-              </p>
-            </div>
             <Search onSearchChange={handleOnSearchChange} />
           </Col>
         </Row>
