056c5f9a4e62c6582970615c06a57024396ccea2
kateewrightt <wrighk@outlook.com>
2024-12-04 17:20:05 +1100

    Preload images and switch Flickr URLs to HTTPS

--- src/components/FlickrGallery.js
+++ src/components/FlickrGallery.js
@@ -4,32 +4,50 @@ import "../assets/stylesheets/CritterModal.css";
 import { fetchConfig } from "../utils/fetchConfig";
 import ThemedSpinner from "./ThemedSpinner";
 
+
 const FlickrGallery = ({ critterName, critterImage, critterType }) => {
   const [flickrImages, setFlickrImages] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
 
   useEffect(() => {
     const fetchFlickrPhotos = async () => {
       try {
-        setLoading(true);
+        setImagesLoaded(false);
         const backendURL = await fetchConfig();
         const response = await fetch(`${backendURL}/critterImages?critterName=${critterName}`);
         if (!response.ok) throw new Error("Failed to fetch critter images");
         const data = await response.json();
-        setFlickrImages(data.images.slice(0, 12)); 
+
+        // Preload images
+        const imageUrls = data.images.slice(0, 12).map(
+          (image) => `https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`
+        );
+
+        await Promise.all(
+          imageUrls.map(
+            (src) =>
+              new Promise((resolve, reject) => {
+                const img = new Image();
+                img.src = src;
+                img.onload = resolve;
+                img.onerror = reject;
+              })
+          )
+        );
+
+        setFlickrImages(data.images.slice(0, 12));
       } catch (error) {
-        console.error("Error fetching Flickr photos:", error);
+        console.error("Error fetching or preloading Flickr photos:", error);
         setFlickrImages([]);
       } finally {
-        setLoading(false);
+        setImagesLoaded(true);
       }
     };
-  
+
     if (critterName) fetchFlickrPhotos();
   }, [critterName]);
 
-  
-  if (loading) {
+  if (!imagesLoaded) {
     return (
       <div className="flickr-gallery-loading">
         <ThemedSpinner critterType={critterType} critterImage={critterImage} />
@@ -49,11 +67,11 @@ const FlickrGallery = ({ critterName, critterImage, critterType }) => {
     <Row className="gallery-container g-0">
       {flickrImages.map((image) => (
         <Col xs={6} sm={5} md={5} lg={3} xl={2} key={image.id}>
-          <a href={`http://www.flickr.com/photos/${image.owner}/${image.id}`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://www.flickr.com/photos/${image.owner}/${image.id}`} target="_blank" rel="noopener noreferrer">
             <div className="flickr-image-container">
               <img
                 className="flickr-image"
-                src={`http://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
+                src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
                 alt={image.title}
               />
             </div>
