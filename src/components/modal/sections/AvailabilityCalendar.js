a2f0f6dc7c652a33157618d2df68694dd135eed3
kateewrightt <wrighk@outlook.com>
2024-12-12 19:51:10 +1100

    Refactor file structure

---
+++ src/components/modal/sections/AvailabilityCalendar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import '../../../assets/stylesheets/Calendar.css';
+
+const months = [
+  "Jan.", "Feb.", "Mar.", "Apr.", "May.", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."
+];
+
+const MonthCalendar = ({ northernMonthsArray = [], southernMonthsArray = [] }) => (
+    <div className="availability-grid">
+      {northernMonthsArray.length > 0 && southernMonthsArray.length > 0 ? (
+        <>
+          <div className="hemisphere-section">
+            <div className="month-grid">
+              {months.map((month, index) => (
+                <div
+                  key={`north-${month}`}
+                  className={`calendar-item ${northernMonthsArray.includes(index + 1) ? "active" : "inactive"}`}
+                >
+                  {month}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="hemisphere-section">
+            <div className="month-grid">
+              {months.map((month, index) => (
+                <div
+                  key={`south-${month}`}
+                  className={`calendar-item ${southernMonthsArray.includes(index + 1) ? "active" : "inactive"}`}
+                >
+                  {month}
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="hemisphere-section">
+          <div className='subheader'>{northernMonthsArray.length > 0 ? "North" : "South"}</div>
+          <div className="month-grid">
+            {months.map((month, index) => (
+              <div
+                key={month}
+                className={`calendar-item ${(northernMonthsArray.length ? northernMonthsArray : southernMonthsArray).includes(index + 1) ? "active" : "inactive"}`}
+              >
+                {month}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+);
+  
+export default MonthCalendar;
