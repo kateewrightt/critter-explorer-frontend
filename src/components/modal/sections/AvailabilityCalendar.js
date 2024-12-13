ad0938271d191bdbb4a60964769441e3ab1284d3
kateewrightt <wrighk@outlook.com>
2024-12-13 13:17:51 +1100

    Add current month indicator

--- src/components/modal/sections/AvailabilityCalendar.js
+++ src/components/modal/sections/AvailabilityCalendar.js
@@ -5,51 +5,57 @@ const months = [
   "Jan.", "Feb.", "Mar.", "Apr.", "May.", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."
 ];
 
-const MonthCalendar = ({ northernMonthsArray = [], southernMonthsArray = [] }) => (
-    <div className="availability-grid">
-      {northernMonthsArray.length > 0 && southernMonthsArray.length > 0 ? (
-        <>
-          <div className="hemisphere-section">
-            <div className="month-grid">
-              {months.map((month, index) => (
-                <div
-                  key={`north-${month}`}
-                  className={`calendar-item ${northernMonthsArray.includes(index + 1) ? "active" : "inactive"}`}
-                >
-                  {month}
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="hemisphere-section">
-            <div className="month-grid">
-              {months.map((month, index) => (
-                <div
-                  key={`south-${month}`}
-                  className={`calendar-item ${southernMonthsArray.includes(index + 1) ? "active" : "inactive"}`}
-                >
-                  {month}
-                </div>
-              ))}
-            </div>
-          </div>
-        </>
-      ) : (
+const MonthCalendar = ({ northernMonthsArray = [], southernMonthsArray = [], currentMonth }) => (
+  <div className="availability-grid">
+    {northernMonthsArray.length > 0 && southernMonthsArray.length > 0 ? (
+      <>
         <div className="hemisphere-section">
-          <div className='subheader'>{northernMonthsArray.length > 0 ? "North" : "South"}</div>
           <div className="month-grid">
             {months.map((month, index) => (
               <div
-                key={month}
-                className={`calendar-item ${(northernMonthsArray.length ? northernMonthsArray : southernMonthsArray).includes(index + 1) ? "active" : "inactive"}`}
+                key={`north-${month}`}
+                className={`calendar-item 
+                  ${northernMonthsArray.includes(index + 1) ? "active" : "inactive"} 
+                  ${currentMonth === index + 1 ? "current-month" : ""}`}
               >
                 {month}
               </div>
             ))}
           </div>
         </div>
-      )}
-    </div>
+        <div className="hemisphere-section">
+          <div className="month-grid">
+            {months.map((month, index) => (
+              <div
+                key={`south-${month}`}
+                className={`calendar-item 
+                  ${southernMonthsArray.includes(index + 1) ? "active" : "inactive"} 
+                  ${currentMonth === index + 1 ? "current-month" : ""}`}
+              >
+                {month}
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    ) : (
+      <div className="hemisphere-section">
+        <div className='subheader'>{northernMonthsArray.length > 0 ? "North" : "South"}</div>
+        <div className="month-grid">
+          {months.map((month, index) => (
+            <div
+              key={month}
+              className={`calendar-item 
+                ${(northernMonthsArray.length ? northernMonthsArray : southernMonthsArray).includes(index + 1) ? "active" : "inactive"} 
+                ${currentMonth === index + 1 ? "current-month" : ""}`}
+            >
+              {month}
+            </div>
+          ))}
+        </div>
+      </div>
+    )}
+  </div>
 );
-  
+
 export default MonthCalendar;
