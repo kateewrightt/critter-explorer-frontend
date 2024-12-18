14b5ff37707281f1f8ddcd004b92b1bb408ecfc4
kateewrightt <wrighk@outlook.com>
2024-12-18 14:05:28 +1100

    Allow user to switch between hemisphere calendars

--- src/components/modal/sections/AvailabilityCalendar.js
+++ src/components/modal/sections/AvailabilityCalendar.js
@@ -1,46 +1,47 @@
 import React from "react";
-import '../../../assets/stylesheets/Calendar.css';
+import "../../../assets/stylesheets/Calendar.css";
 
 const months = [
-  "Jan.", "Feb.", "Mar.", "Apr.", "May.", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."
+  "Jan.", "Feb.", "Mar.", "Apr.", "May.", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec.",
 ];
 
-const MonthCalendar = ({ northernMonthsArray = [], southernMonthsArray = [], currentMonth }) => (
+const MonthCalendar = ({ northernMonthsArray = [], southernMonthsArray = [], currentMonth, showToggle }) => (
   <div className="availability-grid">
-    {northernMonthsArray.length > 0 && southernMonthsArray.length > 0 ? (
+    {showToggle ? (
       <>
         <div className="hemisphere-section">
           <div className="month-grid">
-            {months.map((month, index) => (
-              <div
-                key={`north-${month}`}
-                className={`calendar-item 
-                  ${northernMonthsArray.includes(index + 1) ? "active" : "inactive"} 
-                  ${currentMonth === index + 1 ? "current-month" : ""}`}
-              >
-                {month}
-              </div>
-            ))}
+            {northernMonthsArray.length > 0 &&
+              months.map((month, index) => (
+                <div
+                  key={`north-${month}`}
+                  className={`calendar-item 
+                    ${northernMonthsArray.includes(index + 1) ? "active" : "inactive"} 
+                    ${currentMonth === index + 1 ? "current-month" : ""}`}
+                >
+                  <span>{month}</span>
+                </div>
+              ))}
           </div>
         </div>
         <div className="hemisphere-section">
           <div className="month-grid">
-            {months.map((month, index) => (
-              <div
-                key={`south-${month}`}
-                className={`calendar-item 
-                  ${southernMonthsArray.includes(index + 1) ? "active" : "inactive"} 
-                  ${currentMonth === index + 1 ? "current-month" : ""}`}
-              >
-                {month}
-              </div>
-            ))}
+            {southernMonthsArray.length > 0 &&
+              months.map((month, index) => (
+                <div
+                  key={`south-${month}`}
+                  className={`calendar-item 
+                    ${southernMonthsArray.includes(index + 1) ? "active" : "inactive"} 
+                    ${currentMonth === index + 1 ? "current-month" : ""}`}
+                >
+                  <span>{month}</span>
+                </div>
+              ))}
           </div>
         </div>
       </>
     ) : (
       <div className="hemisphere-section">
-        <div className='subheader'>{northernMonthsArray.length > 0 ? "North" : "South"}</div>
         <div className="month-grid">
           {months.map((month, index) => (
             <div
@@ -49,7 +50,7 @@ const MonthCalendar = ({ northernMonthsArray = [], southernMonthsArray = [], cur
                 ${(northernMonthsArray.length ? northernMonthsArray : southernMonthsArray).includes(index + 1) ? "active" : "inactive"} 
                 ${currentMonth === index + 1 ? "current-month" : ""}`}
             >
-              {month}
+              <span>{month}</span>
             </div>
           ))}
         </div>
